0f2c0f507f7cdbd5fcc3e669bfbc2ccb36be23a7
cutplane1 <snakesnake222@mail.ru>
2023-07-01 21:48:42 +0500

    class

--- index.php
+++ index.php
@@ -1,6 +1,16 @@
 <?php
 
 
+class URouter
+{
+    public array $map;
+
+    public function add($url, $callback)
+    {
+        $map[$url] = $callback;
+    }
+}
+
 preg_match("/^\/users\/(\w+)$/", $_SERVER["REQUEST_URI"], $outr);
 
 var_dump($outr);
