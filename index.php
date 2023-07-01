9201550b64e39ea1cea07e31e4a160623b3c7100
cutplane1 <snakesnake222@mail.ru>
2023-07-01 22:10:31 +0500

    dn1test

--- index.php
+++ index.php
@@ -5,20 +5,28 @@ class URouter
 {
     public array $map;
 
-    public function add($url, $callback)
+    public function add(string $pattern, callable $callback)
     {
-        $map[$url] = $callback;
+        $this->map[$pattern] = ["pattern" => $pattern, "callback" => $callback];
     }
 
     public function dispatch()
     {
-        foreach ($this->map as $url) 
+        foreach ($this->map as $pattern) 
         {
-           echo $url;
+            echo $pattern["pattern"];
         }
     }
 }
 
-preg_match("/^\/users\/(\w+)$/", $_SERVER["REQUEST_URI"], $outr);
+// preg_match("/^\/users\/(\w+)$/", $_SERVER["REQUEST_URI"], $outr);
 
-var_dump($outr);
+// var_dump($outr);
+
+$r = new URouter();
+
+$r->add("/^\/users\/(\w+)$/", function($some_argument) {
+    var_dump($some_argument);
+});
+
+$r->dispatch();
