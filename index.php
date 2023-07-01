a1a9d5347256ab62e04294ddec0f5ccafd8e997d
cutplane1 <snakesnake222@mail.ru>
2023-07-01 21:50:11 +0500

    dntest

--- index.php
+++ index.php
@@ -9,6 +9,14 @@ class URouter
     {
         $map[$url] = $callback;
     }
+
+    public function dispatch()
+    {
+        foreach ($this->map as $url) 
+        {
+           echo $url;
+        }
+    }
 }
 
 preg_match("/^\/users\/(\w+)$/", $_SERVER["REQUEST_URI"], $outr);
