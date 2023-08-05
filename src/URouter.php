3dded14a5ae579bfeae04435c280f400cf0e00ed
cutplane1 <snakesnake222@mail.ru>
2023-08-05 16:19:08 +0500

    code format

--- src/URouter.php
+++ src/URouter.php
@@ -7,45 +7,44 @@ class URouter
     public string $default_req_uri;
 
     public array $routes = [];
+
     public array $middlewares = [];
 
     public function route(string $rule, callable $callback, mixed $middleware = null): URouter
     {
         $pattern = $this->rule2regex($rule);
-        array_push($this->routes, ["pattern" => $pattern, "callback" => $callback, "rule" => $rule, "middleware" => $middleware]);
+        array_push($this->routes, ['pattern' => $pattern, 'callback' => $callback, 'rule' => $rule, 'middleware' => $middleware]);
 
         return $this;
     }
 
     public function rule2regex(string $rule): string
     {
-        $rule = str_replace("/", "\/", $rule);
+        $rule = str_replace('/', "\/", $rule);
         // not int
-        $rule = str_replace("#", "(\w+)", $rule);
+        $rule = str_replace('#', "(\w+)", $rule);
         // int
-        $rule = str_replace("@", "(\d+)", $rule);
+        $rule = str_replace('@', "(\d+)", $rule);
 
-        return "/^". $rule . "$/";
+        return '/^'.$rule.'$/';
     }
 
     public function dispatch(string $uri = null): void
     {
-        if (!$uri) {$uri = $this->default_req_uri;}
-        foreach($this->middlewares as $middleware)
-        {
+        if (!$uri) {
+            $uri = $this->default_req_uri;
+        }
+        foreach ($this->middlewares as $middleware) {
             call_user_func($middleware);
         }
 
-        foreach($this->routes as $route)
-        {
-            if (preg_match($route["pattern"], $uri, $out))
-            {
+        foreach ($this->routes as $route) {
+            if (preg_match($route['pattern'], $uri, $out)) {
                 array_shift($out);
-                if ($route["middleware"])
-                {
-                    call_user_func($route["middleware"]);
+                if ($route['middleware']) {
+                    call_user_func($route['middleware']);
                 }
-                call_user_func_array($route["callback"], $out);
+                call_user_func_array($route['callback'], $out);
             }
         }
     }
@@ -57,25 +56,22 @@ class URouter
         return $this;
     }
 
-    function __construct()
+    public function __construct()
     {
-        if (php_sapi_name() === "cli") 
-        {
-            $this->default_req_uri = "/";
-        } else
-        {
-            $this->default_req_uri = parse_url($_SERVER["REQUEST_URI"], PHP_URL_PATH);
+        if ('cli' === php_sapi_name()) {
+            $this->default_req_uri = '/';
+        } else {
+            $this->default_req_uri = parse_url($_SERVER['REQUEST_URI'], PHP_URL_PATH);
         }
-        
     }
 
-    public function group(array $routes, callable $middleware = null) //: URouter
+    public function group(array $routes, callable $middleware = null) // : URouter
     {
         foreach ($routes as $rule => $callback) {
             $pattern = $this->rule2regex($rule);
-            array_push($this->routes, ["pattern" => $pattern, "callback" => $callback, "rule" => $rule, "middleware" => $middleware]);
+            array_push($this->routes, ['pattern' => $pattern, 'callback' => $callback, 'rule' => $rule, 'middleware' => $middleware]);
         }
-        
+
         return $this;
     }
 }
