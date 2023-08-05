e975becbc98536636fba4e4eb50b62a01bba6a0d
cutplane1 <snakesnake222@mail.ru>
2023-08-05 19:48:06 +0500

    doc

--- src/URouter.php
+++ src/URouter.php
@@ -2,25 +2,74 @@
 
 namespace Cutplane1;
 
+/**
+ * Routing Class.
+ */
 class URouter
 {
+    /**
+     * Error callback.
+     */
     public $error_callback;
 
-    public string $default_req_uri = "/";
+    /**
+     * Request URI.
+     */
+    public string $default_req_uri;
 
+    /**
+     * Invokes a callback.
+     */
     public bool $is_found = false;
 
+    /**
+     * Array of routes.
+     */
     public array $routes = [];
 
+    /**
+     * Array of middlewares.
+     */
     public array $middlewares = [];
 
+    public function __construct()
+    {
+        if ('cli' === php_sapi_name()) {
+            $_SERVER['REQUEST_METHOD'] = "GET";
+            $this->default_req_uri = '/';
+        } else {
+            $this->default_req_uri = parse_url($_SERVER['REQUEST_URI'], PHP_URL_PATH);
+        }
+    }
+
+    /**
+     * GET HTTP Verb.
+     */
     public function get(string $rule, callable $callback, mixed $middleware = null) {$this->route($rule, $callback, $middleware, "GET");}
+    /**
+     * POST HTTP Verb.
+     */
     public function post(string $rule, callable $callback, mixed $middleware = null) {$this->route($rule, $callback, $middleware, "POST");}
+    /**
+     * PUT HTTP Verb.
+     */
     public function put(string $rule, callable $callback, mixed $middleware = null) {$this->route($rule, $callback, $middleware, "PUT");}
+    /**
+     * PATCH HTTP Verb.
+     */
     public function patch(string $rule, callable $callback, mixed $middleware = null) {$this->route($rule, $callback, $middleware, "PATCH");}
+    /**
+     * DELETE HTTP Verb.
+     */
     public function delete(string $rule, callable $callback, mixed $middleware = null) {$this->route($rule, $callback, $middleware, "DELETE");}
+    /**
+     * OPTIONS HTTP Verb.
+     */
     public function options(string $rule, callable $callback, mixed $middleware = null) {$this->route($rule, $callback, $middleware, "OPTIONS");}
     
+    /**
+     * Adds route to array.
+     */
     public function route(string $rule, callable $callback, mixed $middleware = null, string $verb = "GET"): URouter
     {
         $pattern = $this->rule2regex($rule);
@@ -28,7 +77,22 @@ class URouter
 
         return $this;
     }
+    /**
+     * Adds a route group to an array.
+     */
+    public function group(array $routes, callable $middleware = null): URouter
+    {
+        foreach ($routes as $rule => $callback) {
+            $pattern = $this->rule2regex($rule);
+            array_push($this->routes, ['pattern' => $pattern, 'callback' => $callback, 'rule' => $rule, 'middleware' => $middleware]);
+        }
 
+        return $this;
+    }
+
+    /**
+     * Turns an easy-to-read rule into a regular expression.
+     */
     public function rule2regex(string $rule): string
     {
         $rule = str_replace('/', "\/", $rule);
@@ -38,6 +102,40 @@ class URouter
         return '/^'.$rule.'$/';
     }
 
+    /**
+     * Adds middleware to array.
+     */
+    public function middleware(callable $callback): URouter
+    {
+        array_push($this->middlewares, $callback);
+
+        return $this;
+    }
+
+    /**
+     * Executes callback on error.
+     */
+    public function handle_error()
+    {
+        if ($this->error_callback) {
+            call_user_func($this->error_callback);
+        } else {
+            http_response_code(404);
+            echo "404";
+        }
+    }
+
+    /**
+     * Sets error callback.
+     */
+    public function not_found(callable $callback)
+    {
+        $this->error_callback = $callback;
+    }
+
+    /**
+     * Executes a callback when a route is found.
+     */
     public function dispatch(string $uri = null): void
     {
         if (!$uri) {
@@ -59,47 +157,5 @@ class URouter
         }
 
         if (!$this->is_found) {$this->handle_error();}
-    }
-
-    public function middleware(callable $callback): URouter
-    {
-        array_push($this->middlewares, $callback);
-
-        return $this;
-    }
-
-    public function __construct()
-    {
-        if ('cli' === php_sapi_name()) {
-            $_SERVER['REQUEST_METHOD'] = "GET";
-            $this->default_req_uri = '/';
-        } else {
-            $this->default_req_uri = parse_url($_SERVER['REQUEST_URI'], PHP_URL_PATH);
-        }
-    }
-
-    public function group(array $routes, callable $middleware = null): URouter
-    {
-        foreach ($routes as $rule => $callback) {
-            $pattern = $this->rule2regex($rule);
-            array_push($this->routes, ['pattern' => $pattern, 'callback' => $callback, 'rule' => $rule, 'middleware' => $middleware]);
-        }
-
-        return $this;
-    }
-
-    public function handle_error()
-    {
-        if ($this->error_callback) {
-            call_user_func($this->error_callback);
-        } else {
-            http_response_code(404);
-            echo "404";
-        }
-    }
-
-    public function not_found(callable $callback)
-    {
-        $this->error_callback = $callback;
     }
 }
